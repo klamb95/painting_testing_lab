f3c4791967885288f383fd7f830102b43d44356b
klamb95 <klamb1995@gmail.com>
2021-05-04 16:48:38 +0100

    ran out of time

--- decorator.js
+++ decorator.js
@@ -1,9 +1,13 @@
 const Decorator = function(name){
     this.name = name
-    this.paintStock = 0
+    this.paintStock = []
+    this.paintVolumeInStock = 0
 };
 
-
+Decorator.prototype.addToStock = function(paint_can){
+    this.paintStock.push(paint_can)
+    this.paintVolumeInStock += paint_can.volume
+};
 
 
 
--- specs/decorator_specs.js
+++ specs/decorator_specs.js
@@ -1,10 +1,13 @@
 const assert = require('assert')
 const Decorator = require('../decorator.js')
+const PaintCan = require('../paint_can.js')
 
 describe('Decorator', function(){
     let decorator;
     beforeEach(function(){
         decorator = new Decorator('Mark')
+        paintCan = new PaintCan(2)
+        
     })
 
     it('should have a name', function(){
@@ -17,5 +20,17 @@ describe('Decorator', function(){
         assert.strictEqual(actual, 0)
     })
 
-    it('should')
+    it('should be able to add to paint stock', function(){
+        decorator.addToStock(paintCan);
+        // const expected = [paint_can]
+        const actual = decorator.paintStock;
+        assert.strictEqual(actual, 1)
+    })
+
+    xit('should be able to count how much paint is in stock', function(){
+        decorator.addToStock(paintCan);
+        decorator.paintTotalVolume();
+        const actual = decorator.paintStock;
+        assert.strictEqual(actual, 2)
+    })
 })
